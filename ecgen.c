334e31ad12911073318af8641a5b7aeedd992d0a
J08nY <johny@neuromancer.sk>
2016-12-22 00:23:13 +0100

    ecgen: some docs

--- ecgen.c
+++ ecgen.c
@@ -1,22 +1,41 @@
-
-#include <pari/pari.h>
-
-#include <stdio.h>
-#include <readline/readline.h>
+/**
+ * @author J08nY
+ * @license GPL v2.0
+ * @version 0.1
+ */
 
 #include "gp.h"
 
+#include <pari/pari.h>
+
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ *
+ *
+ */
 char *readHex(const char* prompt) {
     char *r = readline(prompt);
     size_t len = strlen(r);
+    if (len > 2 && r[0] == '0' && (r[1] == 'x' || r[1] == 'X')){
+	return r;
+    }
+
     char *result = realloc(r, len + 3);
-    memmove(result + 2, result, len);
+    memmove(result + 2, result, len + 1);
     result[0] = '0';
     result[1] = 'x';
-    result[len + 2] = 0;
     return result;
 }
 
+/**
+ *
+ *
+ *
+ */
 int main(int argc, char * argv[]) {
 	GEN (*todo)(GEN,GEN,GEN,long) = &largest_prime;
 	void (*print)(GEN) = &print_params;
@@ -36,7 +55,7 @@ int main(int argc, char * argv[]) {
 
 	init_gp();
 	pari_init( 1e9, 1e8 );
-	
+
 	//default0("datadir","./data");
 
 	char *sp = readHex("p:");
