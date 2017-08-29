9988d8068461550e7b303a4cad46926aa27bf8dd
J08nY <johny@neuromancer.sk>
2017-08-30 01:02:07 +0200

    Add a test for subgroups_all of a curve with two generators.

--- test/src/math/test_subgroups.c
+++ test/src/math/test_subgroups.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2017 J08nY
  */
 #include "gen/types.h"
+#include "gen/point.h"
 #include "math/subgroups.h"
 #include <criterion/criterion.h>
 #include "test/utils.h"
@@ -50,4 +51,25 @@ Test(subgroups, test_all_factors) {
 	GEN divs = subgroups_all(&curve, &cfg);
 	GEN vec = mkvec3s(3, 9, 27);
 	cr_assert(gequal(divs, vec), "Factors not equal!");
+}
+
+Test(subgroups, test_all_factors_two_gens) {
+	// curve = ellinit([2, 3], 23), order = 24
+	point_t **gens = points_new(2);
+	gens[0] = point_new();
+	gens[0]->order = stoi(12);
+	gens[0]->cofactor = stoi(2);
+	gens[0]->point = mkvec2(mkintmodu(6, 23), mkintmodu(1, 23));
+	gens[1] = point_new();
+	gens[1]->order = stoi(6);
+	gens[1]->cofactor = stoi(4);
+	gens[1]->point = mkvec2(mkintmodu(20, 23), mkintmodu(19, 23));
+
+	curve_t curve = {.order = stoi(24), .ngens = 2, .generators = gens};
+	config_t cfg = {.prime = false};
+	GEN divs = subgroups_all(&curve, &cfg);
+	GEN vec = mkvecn(8, stoi(2), stoi(3), stoi(4), stoi(6), stoi(12), stoi(2),
+					 stoi(3), stoi(6));
+	cr_assert(gequal(divs, vec), "Factors not equal!");
+	points_free_deep(&gens, 2);
 }
