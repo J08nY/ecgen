c6bc5bf67cd90110ba263867975f79f1365443c0
J08nY <johny@neuromancer.sk>
2021-01-09 23:22:14 +0100

    Fix compat pari version check.

--- src/misc/compat.h
+++ src/misc/compat.h
@@ -12,8 +12,14 @@
 #define PARI_VERSION_MINOR (((PARI_VERSION_CODE) >> 8) & 0xff)
 #define PARI_VERSION_MAJOR (PARI_VERSION_CODE >> 16)
 
-#if !(((PARI_VERSION_MAJOR == 2) && (PARI_VERSION_MINOR == 12) && (PARI_VERSION_PATCH >= 2)) || ((PARI_VERSION_MAJOR >= 2) && (PARI_VERSION_MINOR >= 12)))
-// pari < 2.12.2
+#define PARI_VERSION_GT(a,b,c) ((PARI_VERSION_MAJOR == a && PARI_VERSION_MINOR == b && PARI_VERSION_PATCH > c) || (PARI_VERSION_MAJOR == a && PARI_VERSION_MINOR > b) || (PARI_VERSION_MAJOR > a))
+#define PARI_VERSION_EQ(a,b,c) (PARI_VERSION_MAJOR == a && PARI_VERSION_MINOR == b && PARI_VERSION_PATCH == c)
+#define PARI_VERSION_GE(a,b,c) (PARI_VERSION_GT(a,b,c) || PARI_VERSION_EQ(a,b,c))
+#define PARI_VERSION_LT(a,b,c) !(PARI_VERSION_GE(a,b,c))
+#define PARI_VERSION_LE(a,b,c) !(PARI_VERSION_GT(a,b,c))
+
+
+#if PARI_VERSION_LT(2,12,1)
 #define polisirreducible isirreducible
 #endif
 
