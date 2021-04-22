3e1aefd54ed04787d6ef8b8648b582e563554819
J08nY <johny@neuromancer.sk>
2021-04-22 12:44:46 +0200

    Fix CM-discriminant and conductor computation.

--- src/gen/metadata.c
+++ src/gen/metadata.c
@@ -21,13 +21,13 @@ GENERATOR(metadata_gen) {
 	} else {
 		return -7;
 	}
-	GEN frobenius = negi(subii(curve->order, addis(q, 1)));
+	GEN frobenius = subii(addis(q, 1), curve->order);
 	GEN conductor;
 	GEN cm_disc;
 	if (typ(curve->field) == t_INT) {
-		GEN full_disc = subii(mulis(q, 4), sqri(frobenius));
-		cm_disc = core(full_disc);
-		conductor = sqrti(diviiexact(full_disc, cm_disc));
+		GEN d_f = coredisc2(subii(sqri(frobenius), mulis(q, 4)));
+		cm_disc = gcopy(gel(d_f, 1));
+		conductor = gcopy(gel(d_f, 2));
 		gerepileall(ltop, 6, &j, &disc, &embedding_degree, &frobenius, &cm_disc, &conductor);
 	} else if (typ(curve->field) == t_FFELT) {
 		cm_disc = NULL;
