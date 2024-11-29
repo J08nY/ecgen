cc31771532bd2ffad92fbc0d3832e55621c47ddb
J08nY <johny@neuromancer.sk>
2024-11-29 21:24:24 +0100

    Bump criterion (uses meson now).

--- test/criterion.sh
+++ test/criterion.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ ! -f "lib/criterion/CMakeLists.txt" ]; then
-	echo "lib/criterion is missing. It is a git submodule from https://github.com/Snaipe/Criterion at a64b860.";
+if [ ! -f "lib/criterion/meson.build" ]; then
+	echo "lib/criterion is missing. It is a git submodule from https://github.com/Snaipe/Criterion at 9c01cbe.";
 	exit 1;
 fi
 
 cd lib/criterion
-mkdir -p build
-cd build
-cmake ..  >/dev/null
-cmake --build . >/dev/null
+meson setup build
+meson compile -C build
